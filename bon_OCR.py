07150d7e87b1e3c1c8bd811ea9447de7e918c83e
Suzanne <suzanne@ekhart.eu>
2021-08-20 23:05:29 +0200

    More testing

--- bon_OCR.py
+++ bon_OCR.py
@@ -1,4 +1,4 @@
-import easyocr
+#import easyocr
 import os
 import math
 from typing import Tuple, Union
@@ -11,7 +11,7 @@ from deskew import determine_skew
 import pytesseract as pytess
 print(os.getcwd())
 
-
+# roteet
 def rotate(image: np.ndarray, angle: float, background: Union[int, Tuple[int, int, int]]) -> np.ndarray:
     old_width, old_height = image.shape[:2]
     angle_radian = math.radians(angle)
@@ -25,15 +25,27 @@ def rotate(image: np.ndarray, angle: float, background: Union[int, Tuple[int, in
     return cv2.warpAffine(image, rot_mat, (int(round(height)), int(round(width))), borderValue=background)
 
 
-image = cv2.imread('test_data/ah_1.jpg')
+# noise removal
+def remove_noise(image):
+    return cv2.medianBlur(image,5)
+
+
+# Picture in variable
+image = cv2.imread('test_data/ah_5_cropped.jpg')
+#noise removal
+image = remove_noise(image)
+# Make grayscale of picture
 grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# Determine angle of skew
 angle = determine_skew(grayscale) + 90
+# Rotate the grayscaled image based on angle
 rotated = rotate(grayscale, angle, (0, 0, 0))
 image = rotated
-#image = cv2.threshold(rotated, 100,255,cv2.THRESH_BINARY_INV)[1]
+# Threshold image
+image = cv2.threshold(image, 150 ,255,cv2.THRESH_BINARY)[1]
 
-cv2.imwrite('test_data/ah_4_rotated.jpg', image)
-
+# Write new image to new file
+cv2.imwrite('test_data/output.jpg', image)
 
 #reader = easyocr.Reader(['nl',]) # need to run only once to load model into memory
 #result = reader.readtext(image, paragraph=False, detail=0)
@@ -41,7 +53,8 @@ cv2.imwrite('test_data/ah_4_rotated.jpg', image)
 #https://github.com/JaidedAI/EasyOCR
 
 bon_out = pytess.image_to_data(image, lang="nld", output_type=pytess.Output.DATAFRAME)
-print(bon_out)
+#print(bon_out)
+
 # disable dicts: https://tesseract-ocr.github.io/tessdoc/ImproveQuality.html#dictionaries-word-lists-and-patterns
 data = bon_out[["page_num", "block_num", "par_num", "line_num", "word_num", "conf", "text", "left", "width", "top", "height"]]
 data = data[data["conf"] > 0]
